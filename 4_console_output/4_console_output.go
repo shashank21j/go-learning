5958c5a680451a258a3f8477fbf40ba65cffaaa9
Shashank Sharma <s21j@pm.me>
2024-03-21 10:20:16 +0100

    u

--- 4_console_output/4_console_output.go
+++ 4_console_output/4_console_output.go
@@ -2,8 +2,31 @@ package main
 
 import "fmt"
 
+func ExampleFunction(x string, y int) (string, int) {
+	return x, y
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	// Print line
+	fmt.Println("Hello, World!", 123)
+
+	// Print formatted
+	fmt.Printf("Decimal:%d, Binary:%b, Scientific:%e\n", 10, 10, 10.0)
+
+	// Print string and float
+	fmt.Printf("String:%s, Float:%.2f\n", "Hello, World!", 10.0)
+
+	// Print integer with padding
+	fmt.Printf("Padded integer:%05d\n", 10)
+
+	// Escape characters
+	fmt.Printf("Escaped characters\"%%1\"\n")
+
+	// String formatting
 	s := fmt.Sprintf("Hello, World!")
 	fmt.Println(s)
+
+	// Function call
+	x, y := ExampleFunction("Hello, World!", 123)
+	fmt.Println(x, y)
 }

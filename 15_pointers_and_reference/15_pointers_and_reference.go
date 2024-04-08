7fea563dd04d90582f6a9b9bf538447380faeb59
Shashank Sharma <s21j@pm.me>
2024-04-08 07:57:15 +0530

    15

---
+++ 15_pointers_and_reference/15_pointers_and_reference.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type Book struct {
+	title  string
+	author string
+}
+
+func changeTitle(book *Book) {
+	book.title = "New Title"
+}
+
+func change(s []int) {
+	s[0] = 100
+}
+
+func change_int(num *int) {
+	*num = 20
+}
+
+func main() {
+	x := 2
+	x_ptr := &x
+	fmt.Println("x: ", x, "x_ptr: ", x_ptr)
+
+	var y_ptr *int = &x
+	fmt.Println("y_ptr: ", y_ptr, "*y_ptr: ", *y_ptr)
+
+	s := []int{1, 2, 3, 4, 5}
+	change(s)
+	fmt.Println("s: ", s)
+
+	num := 10
+	change_int(&num)
+	fmt.Println("num: ", num)
+
+	x_1 := []string{"a", "b", "c"}
+	x_2 := &x_1
+	x_3 := &x_2
+	fmt.Println("x_1: ", x_1, "*x_2: ", *x_2, "**x_3: ", **x_3)
+
+	book := Book{"Go Programming", "John Doe"}
+	book_ptr := &book
+	fmt.Println("book: ", book, "book_ptr: ", book_ptr)
+	changeTitle(book_ptr)
+	fmt.Println("book: ", book, "book_ptr: ", book_ptr)
+
+}
